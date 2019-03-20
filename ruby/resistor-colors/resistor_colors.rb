acfea2a4825f539e957cd4d5ea3d5edf6d4cef58
joshsarna <joshsarna@gmail.com>
2019-03-19 18:33:11 -0700

    finish resistor_colors on ruby track

---
+++ ruby/resistor-colors/resistor_colors.rb
@@ -0,0 +1,23 @@
+class ResistorColors
+  @@colors = {
+    "black" => 0,
+    "brown" => 1,
+    "red" => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green" => 5,
+    "blue" => 6,
+    "violet" => 7,
+    "grey" => 8,
+    "white" => 9
+  }
+
+  def self.value(colors)
+    value = 0
+    colors.each do |color|
+      value *= 10
+      value += @@colors[color]
+    end
+    value
+  end
+end
